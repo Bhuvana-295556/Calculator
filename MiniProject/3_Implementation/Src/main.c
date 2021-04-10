c9b178cd448429c35a75fe9261c454ffad045ef3
Bhuvana-295556 <82052062+Bhuvana-295556@users.noreply.github.com>
2021-04-10 11:15:40 +0530

    Update main.c

--- MiniProject/3_Implementation/Src/main.c
+++ MiniProject/3_Implementation/Src/main.c
@@ -1,1 +1,78 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+#define note "maze mat lo, enter the valid operation"
+void addition();
+void subtraction();
+void multiplication();
+void division();
+void modulus();
+void factorial();
+void power();
+void square();
+void cube();
+void squareroot();
+
+int main(){
+    printf("\t\tWelcome to the scientific calculator!!\n\n");
+    int choice;
+    printf("\n******* Press 0 to quit the program ********\n");
+    printf("Enter 1 for Addition \n");
+    printf("Enter 2 for Subtraction \n");
+    printf("Enter 3 for Multiplication \n");
+    printf("Enter 4 for Division \n");
+    printf("Enter 5 for Modulus\n");
+    printf("Enter 6 for Power \n");
+    printf("Enter 7 for Factorial \n");
+    printf("Enter 8  for square \n");
+    printf("Enter 9  for cube \n");
+    printf("Enter 10 for squareroot\n\n");
+    
+    while(1){    
+    printf("\n\nEnter the operation you want to do: ");
+    
+    scanf("%d",&choice);
+            
+    switch(choice)
+    {
+                case 1:
+                    addition();
+                    break;
+                case 2:
+                    subtraction();
+                    break;
+                case 3:
+                    multiplication();
+                    break;
+                case 4:
+                    division();
+                    break;
+                case 5:
+                    modulus();
+                    break;
+                case 6:
+                    power();
+                    break;
+                case 7:
+                    factorial();
+                    break;
+                case 8:
+                    square();
+                    break;
+                case 9:
+                    cube();
+                    break;
+                case 10:
+                    squareroot();
+                    break;
+                case 0:
+                    exit(0);
+                default:
+                    printf("\n********** %s ***********\n",note);
+        }
+    
+    }
+    return 0;
+}
 
