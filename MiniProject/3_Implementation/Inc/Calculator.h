39723d40d0ab573ff4df788e98cbb95529dfc9dd
Bhuvana-295556 <82052062+Bhuvana-295556@users.noreply.github.com>
2021-04-15 18:11:01 +0530

    Update Calculator.h

--- MiniProject/3_Implementation/Inc/Calculator.h
+++ MiniProject/3_Implementation/Inc/Calculator.h
@@ -9,9 +9,7 @@ void division(int number1,int number2);
 void modulus(int number1,int number2);
 void factorial(int number1);
 void power(int number1);
-void square(int number1);
 void cube(int number1);
-void squareroot(int number1);
 void cuberoot(int number1);
 
 #endif
